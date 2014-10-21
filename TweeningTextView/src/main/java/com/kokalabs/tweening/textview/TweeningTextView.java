5bed32b5ded62d129560ecc75d2345722347c2c1
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-10-21 17:07:04 -0400

    Adds template for textView

---
+++ TweeningTextView/src/main/java/com/kokalabs/tweening/textview/TweeningTextView.java
@@ -0,0 +1,25 @@
+package com.kokalabs.tweening.textview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class TweeningTextView extends View {
+    public TweeningTextView(Context context) {
+        super(context);
+    }
+
+    public TweeningTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TweeningTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
