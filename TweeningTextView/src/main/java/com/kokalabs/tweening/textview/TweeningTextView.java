0d3c1610062857a9de7d7cd39ea1673e4a4cd1a3
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-10-21 17:13:41 -0400

    Adds onMeasure calculation

--- TweeningTextView/src/main/java/com/kokalabs/tweening/textview/TweeningTextView.java
+++ TweeningTextView/src/main/java/com/kokalabs/tweening/textview/TweeningTextView.java
@@ -6,6 +6,8 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class TweeningTextView extends View {
+    private static final double SCALE = 0.5;
+
     public TweeningTextView(Context context) {
         super(context);
     }
@@ -21,5 +23,22 @@ public class TweeningTextView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int widthWithoutPadding = width - getPaddingLeft() - getPaddingRight();
+        int heightWithoutPadding = height - getPaddingTop() - getPaddingBottom();
+        int maxWidth = (int) (heightWithoutPadding * SCALE);
+        int maxHeight = (int) (widthWithoutPadding / SCALE);
+        if (widthWithoutPadding > maxWidth) {
+            width = maxWidth + getPaddingLeft() + getPaddingRight();
+        } else {
+            height = maxHeight + getPaddingTop() + getPaddingBottom();
+        }
+        setMeasuredDimension(width, height);
     }
 }
