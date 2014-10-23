8116b83f167354b592431325af5fd02ba921bda8
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2014-10-22 23:40:58 -0400

    Adds support for full upper case alphabet

--- TweeningTextViewExample/src/main/java/com/kokalabs/tweening/textview/example/Char.java
+++ TweeningTextViewExample/src/main/java/com/kokalabs/tweening/textview/example/Char.java
@@ -8,6 +8,27 @@ public class Char {
     private static final String C = "M129 735q0 223 84.5 393t243 262.5t368.5 92.5q214 0 383 -80l-41 -92q-160 80 -336 80q-275 0 -433 -176t-158 -482q0 -313 149 -486t426 -173q184 0 338 47v-90q-145 -51 -362 -51q-308 0 -485 199t-177 556z";
     private static final String D = "M1317 745q0 -368 -193 -556.5t-567 -188.5h-350v1462h395q350 0 532.5 -183t182.5 -534zM1206 741q0 314 -159.5 472.5t-468.5 158.5h-269v-1282h242q655 0 655 651z";
     private static final String E = "M207 0v1462h799v-94h-697v-553h658v-94h-658v-627h697v-94h-799z";
+    private static final String F = "M207 0v1462h801v-94h-699v-620h660v-95h-660v-653h-102z";
+    private static final String G = "M129 729q0 223 91.5 395.5t262 266.5t391.5 94q239 0 429 -88l-41 -92q-190 88 -394 88q-289 0 -458.5 -178.5t-169.5 -481.5q0 -330 161 -496.5t473 -166.5q202 0 343 57v514h-435v96h539v-667q-212 -90 -477 -90q-346 0 -530.5 195.5t-184.5 553.5z";
+    private static final String H = "M207 0v1462h102v-649h854v649h103v-1462h-103v719h-854v-719h-102z";
+    private static final String I = "M207 0v1462h102v-1462h-102z";
+    private static final String J = "M-184 -254q78 -20 149 -20q242 0 242 264v1472h102v-1462q0 -369 -342 -369q-92 0 -151 27v88z";
+    private static final String K = "M207 0v1462h102v-760l162 162l573 598h130l-599 -618l615 -844h-125l-561 772l-195 -172v-600h-102z";
+    private static final String L = "M207 0v1462h102v-1366h697v-96h-799z";
+    private static final String M = "M207 0v1462h158l518 -1286h6l518 1286h154v-1462h-103v1108q0 116 12 240h-8l-547 -1348h-65l-545 1350h-8q8 -124 8 -254v-1096h-98z";
+    private static final String N = "M207 0v1462h102l865 -1296h6q-9 180 -9 342v954h99v-1462h-103l-866 1298h-8q12 -232 12 -350v-948h-98z";
+    private static final String O = "M129 735q0 349 175.5 549.5t479.5 200.5q306 0 479 -201.5t173 -550.5q0 -348 -174 -550.5t-480 -202.5q-305 0 -479 202.5t-174 552.5zM240 733q0 -314 140 -485.5t402 -171.5q264 0 403.5 170t139.5 487q0 316 -139.5 484.5t-401.5 168.5q-261 0 -402.5 -170 t-141.5 -483z";
+    private static final String P = "M207 0v1462h358q522 0 522 -420q0 -212 -144 -325t-408 -113h-226v-604h-102zM309 692h201q247 0 357 81.5t110 264.5q0 169 -104 250.5t-322 81.5h-242v-678z";
+    private static final String Q = "M129 735q0 349 175.5 549.5t479.5 200.5q306 0 479 -201.5t173 -550.5q0 -294 -126 -486.5t-349 -246.5l333 -348h-166l-282 330l-33 -2h-31q-305 0 -479 202.5t-174 552.5zM240 733q0 -314 140 -485.5t402 -171.5q264 0 403.5 170t139.5 487q0 316 -139.5 484.5 t-401.5 168.5q-261 0 -402.5 -170t-141.5 -483z";
+    private static final String R = "M207 0v1462h348q272 0 402 -100.5t130 -302.5q0 -147 -77.5 -248t-235.5 -145l397 -666h-122l-377 637h-363v-637h-102zM309 725h279q185 0 287 82.5t102 243.5q0 167 -100 243t-326 76h-242v-645z";
+    private static final String S = "M111 39v102q158 -67 403 -67q180 0 285.5 82.5t105.5 216.5q0 83 -35 137.5t-114 99.5t-232 97q-224 77 -309.5 166.5t-85.5 238.5q0 164 128.5 267.5t330.5 103.5q206 0 387 -78l-37 -88q-182 76 -348 76q-162 0 -258 -75t-96 -204q0 -81 29.5 -133t96.5 -93.5 t230 -99.5q171 -59 257 -114.5t125.5 -126t39.5 -170.5q0 -183 -134.5 -290t-357.5 -107q-268 0 -411 59z";
+    private static final String T = "M10 1366v96h1053v-96h-475v-1366h-103v1366h-475z";
+    private static final String U = "M190 520v942h103v-946q0 -211 117 -328.5t331 -117.5q209 0 324 115.5t115 320.5v956h102v-946q0 -252 -146 -394t-407 -142q-254 0 -396.5 142.5t-142.5 397.5z";
+    private static final String V = "M0 1462h109l368 -995q84 -225 113 -338q20 75 79 233l402 1100h111l-547 -1462h-90z";
+    private static final String W = "M51 1462h107l256 -942q15 -57 28 -105.5t23.5 -91t19 -82t15.5 -79.5q24 136 102 413l250 887h113l293 -1018q51 -176 73 -284q13 72 33.5 153t308.5 1149h103l-404 -1462h-84l-321 1128q-40 139 -60 228q-16 -87 -45.5 -200t-322.5 -1156h-86z";
+    private static final String X = "M0 0l492 762l-447 700h115l395 -626l401 626h109l-453 -698l490 -764h-117l-432 682l-440 -682h-113z";
+    private static final String Y = "M0 1462h117l426 -800l428 800h110l-487 -897v-565h-105v557z";
+    private static final String Z = "M82 0v76l856 1290h-817v96h954v-76l-858 -1290h881v-96h-1016z";
 
     static final SvgPath from = SvgPath.from(E, 2048);
     static final SvgPath to = SvgPath.from(C, 2048);
@@ -24,6 +45,48 @@ public class Char {
                 return SvgPath.from(D, 2048);
             case 4:
                 return SvgPath.from(E, 2048);
+            case 5:
+                return SvgPath.from(F, 2048);
+            case 6:
+                return SvgPath.from(G, 2048);
+            case 7:
+                return SvgPath.from(H, 2048);
+            case 8:
+                return SvgPath.from(I, 2048);
+            case 9:
+                return SvgPath.from(J, 2048);
+            case 10:
+                return SvgPath.from(K, 2048);
+            case 11:
+                return SvgPath.from(L, 2048);
+            case 12:
+                return SvgPath.from(M, 2048);
+            case 13:
+                return SvgPath.from(N, 2048);
+            case 14:
+                return SvgPath.from(O, 2048);
+            case 15:
+                return SvgPath.from(P, 2048);
+            case 16:
+                return SvgPath.from(Q, 2048);
+            case 17:
+                return SvgPath.from(R, 2048);
+            case 18:
+                return SvgPath.from(S, 2048);
+            case 19:
+                return SvgPath.from(T, 2048);
+            case 20:
+                return SvgPath.from(U, 2048);
+            case 21:
+                return SvgPath.from(V, 2048);
+            case 22:
+                return SvgPath.from(W, 2048);
+            case 23:
+                return SvgPath.from(X, 2048);
+            case 24:
+                return SvgPath.from(Y, 2048);
+            case 25:
+                return SvgPath.from(Z, 2048);
             default:
                 return SvgPath.from(A, 2048);
         }
